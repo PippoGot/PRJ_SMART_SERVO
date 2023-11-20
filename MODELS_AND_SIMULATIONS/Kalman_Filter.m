ed06c2132e29096c548fa655a9216b2986243c12
giuliapegoraro3 <129194662+giuliapegoraro3@users.noreply.github.com>
2023-11-20 18:03:16 +0100

    Kalman problems!!!

---
+++ MODELS_AND_SIMULATIONS/Kalman_Filter.m
@@ -0,0 +1,42 @@
+%% Init
+
+clc
+close all
+
+Full_Model_params;
+
+
+%% State space model
+A = [0, motor.gearbox, 0; 0, -motor.B/motor.J, motor.Kphi/motor.J; 0, -motor.Kphi/motor.La, -motor.Ra/motor.La];
+B = [0; 0; 1/motor.La];
+C = [0, 0, 1; 1, 0, 0];
+D = zeros(size(C,1),size(B,2));
+
+Q = 0*eye(3);
+R = eye(2);
+
+sys = ss(A,B,C,D);
+dsys = c2d(sys, uc.Ts, 'zoh');
+
+[dA, dB, dC, dD] = ssdata(dsys);
+
+
+%% Kalman Filter Pillonetto
+
+P = A*P
+
+
+%% Kalman filter 
+% Q = [0 0 0; 0 0 0; 0 0 1];
+% R = eye(2);
+% N = zeros(3,2);
+
+
+% Kf = lqr(A',C',Q,R)
+
+% [Kf, P, E] = lqe(A, Q, C, Q, R, N)
+
+
+% [Kf_curr,P_curr,E_curr] = lqe(A, Q, C_curr, Q, R);
+% 
+% [Kf_pos,P_pos,E_pos] = lqe(A, Q, C_pos, Q, R);
