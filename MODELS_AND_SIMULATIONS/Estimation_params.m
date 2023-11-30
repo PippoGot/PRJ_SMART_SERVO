2c5c9a9c567d2c498487b926302af54fd33da542
filippobertin <filippo.bertin.1@studenti.unipd.it>
2023-11-30 22:47:18 +0100

    Aggiunto file simulink per la simulazione della stima di J
    Aggiunto file simulink per la simulazione della stima di J

--- MODELS_AND_SIMULATIONS/Estimation_params.m
+++ MODELS_AND_SIMULATIONS/Estimation_params.m
@@ -11,8 +11,8 @@ Full_Model_params;
 %Low-pass filter current Hi
 H.wc = 2*pi*20;
 H.delta = 1/sqrt(2);
-Hi.num = est.H.wc^2;
-Hi.den = [1 2*est.H.delta*est.H.wc est.H.wc^2];
+Hi.num = H.wc^2;
+Hi.den = [1 2*H.delta*H.wc H.wc^2];
 
 %High-pass filter velocity and acceleration Hw
 Hw.num = [H.wc^2 0];
@@ -28,13 +28,13 @@ Hw.den = [1 2*H.delta*H.wc H.wc^2];
 
 t = 0.01;       % Time step of the simulation [s] (Probably to cange)
 t_tran1 = 0.5;  % Time of transient smaples to delate in experiment 1
-t_delta1 = 5;    % Period of the step in experiment 1
+t_delta1 = 2;   % Period of the step in experiment 1
 M = 8;          % Number of step of the steircase function reference (To use on simulink) (maybe not necessary)
 
 for n = 1:M
-    steady.I(1+(n-1)*(t_delta1-2*t_tran1)/t:n*(t_delta1-2*t_tran1)/t,1) = I.signals.values((t_tran1+(n-1)*t_delta1)/t:(t_delta1-t_tran1+(n-1)*t_delta1)/t,1);    % Discard  transient, leaving only steady state samples
-    steady.V(1+(n-1)*(t_delta1-2*t_tran1)/t:n*(t_delta1-2*t_tran1)/t,1) = V.signals.values((t_tran1+(n-1)*t_delta1)/t:(t_delta1-t_tran1+(n-1)*t_delta1)/t,1);
-    steady.wm(1+(n-1)*(t_delta1-2*t_tran1)/t:n*(t_delta1-2*t_tran1)/t,1) = wm.signals.values((t_tran1+(n-1)*t_delta1)/t:(t_delta1-t_tran1+(n-1)*t_delta1)/t,1);
+    steady.I(1+(n-1)*(t_delta1-2*t_tran1)/t:n*(t_delta1-2*t_tran1)/t,1) = out.I.signals.values((t_tran1+(n-1)*t_delta1)/t:(t_delta1-t_tran1+(n-1)*t_delta1)/t,1);    % Discard  transient, leaving only steady state samples
+    steady.V(1+(n-1)*(t_delta1-2*t_tran1)/t:n*(t_delta1-2*t_tran1)/t,1) = out.V.signals.values((t_tran1+(n-1)*t_delta1)/t:(t_delta1-t_tran1+(n-1)*t_delta1)/t,1);
+    steady.wm(1+(n-1)*(t_delta1-2*t_tran1)/t:n*(t_delta1-2*t_tran1)/t,1) = out.wm.signals.values((t_tran1+(n-1)*t_delta1)/t:(t_delta1-t_tran1+(n-1)*t_delta1)/t,1);
 end
 
 for n = 1:M
@@ -70,10 +70,10 @@ est.tau_s = Ls.theta(2,1);
 t_tran2 = 0.1;  % Time of transient smaples to delate in experiment 2
 t_delta2 = 1;    % Period of the step in experiment 2
 
-dyn.I(1+(n-1)*(t_delta2-2*t_tran2)/t:n*(t_delta2-2*t_tran2)/t,1) = I.signals.values((t_tran2+(n-1)*t_delta2)/t:(t_delta2-t_tran2+(n-1)*t_delta2)/t,1);     % Discard  transient (dyn = dynamic)
-dyn.V(1+(n-1)*(t_delta2-2*t_tran2)/t:n*(t_delta2-2*t_tran2)/t,1) = V.signals.values((t_tran2+(n-1)*t_delta2)/t:(t_delta2-t_tran2+(n-1)*t_delta2)/t,1);
-dyn.wm(1+(n-1)*(t_delta2-2*t_tran2)/t:n*(t_delta2-2*t_tran2)/t,1) = wm.signals.values((t_tran2+(n-1)*t_delta2)/t:(t_delta2-t_tran2+(n-1)*t_delta2)/t,1);
-dyn.acc(1+(n-1)*(t_delta2-2*t_tran2)/t:n*(t_delta2-2*t_tran2)/t,1) = acc.signals.values((t_tran2+(n-1)*t_delta2)/t:(t_delta2-t_tran2+(n-1)*t_delta2)/t,1);
+dyn.I(1+(n-1)*(t_delta2-2*t_tran2)/t:n*(t_delta2-2*t_tran2)/t,1) = out.I.signals.values((t_tran2+(n-1)*t_delta2)/t:(t_delta2-t_tran2+(n-1)*t_delta2)/t,1);     % Discard  transient (dyn = dynamic)
+dyn.V(1+(n-1)*(t_delta2-2*t_tran2)/t:n*(t_delta2-2*t_tran2)/t,1) = out.V.signals.values((t_tran2+(n-1)*t_delta2)/t:(t_delta2-t_tran2+(n-1)*t_delta2)/t,1);
+dyn.wm(1+(n-1)*(t_delta2-2*t_tran2)/t:n*(t_delta2-2*t_tran2)/t,1) = out.wm.signals.values((t_tran2+(n-1)*t_delta2)/t:(t_delta2-t_tran2+(n-1)*t_delta2)/t,1);
+dyn.acc(1+(n-1)*(t_delta2-2*t_tran2)/t:n*(t_delta2-2*t_tran2)/t,1) = out.acc.signals.values((t_tran2+(n-1)*t_delta2)/t:(t_delta2-t_tran2+(n-1)*t_delta2)/t,1);
 
 dyn.tau_m = est.Kphi * dyn.I;
 dyn.tau_f = est.B * dyn.wm + est.tau_s/motor.gearbox * sign(dyn.wm); % possibly with gear reatio (est.tau_s / N * sign(dyn.wm))
