7ab5d824481f59ea9cbd721df3ed832dee2fb6f4
Filippo Gottardo <11got200@gmail.com>
2023-12-01 21:24:08 +0100

    commentato modulo I2C Device

--- SOURCE/Core/Inc/i2c_device.hpp
+++ SOURCE/Core/Inc/i2c_device.hpp
@@ -13,51 +13,69 @@
 
 
 
-// --- I2C device class -----------------------------------------------------------------
+// ----------------------------------------------------- I2C_Device class declaration ---
 
 class I2C_Device {
 
 public:
-	// Constructor
+	// --- Device constructor -----------------------------------------------------------
+
 	I2C_Device(
 			I2C_HandleTypeDef *device_handle,
 			uint8_t device_address,
 			uint32_t response_delay = HAL_MAX_DELAY
 			);
 
-	// Getter methods
+
+	// --- Getter methods ---------------------------------------------------------------
+
 	uint8_t getDeviceAddress(void){ return I2C_Device::_device_address >> 1; };
+
 	I2C_HandleTypeDef getDeviceHandle(void){ return *I2C_Device::_device_handle; };
+
 	uint32_t getResponseDelay(void){ return I2C_Device::_response_delay; };
 
-	// Boolean methods
+
+	// --- Utility methods --------------------------------------------------------------
+
 	bool isConnected(void);
 
+
 protected:
-	// Variables
+	// --- Variables --------------------------------------------------------------------
+
 	I2C_HandleTypeDef *_device_handle;	// Bus to which the device is connected to
 	const uint8_t _device_address; 		// I2C address of the device
 	const uint32_t _response_delay;		// Response delay used in read-write operations
 
-	// Low-level I2C methods
-	HAL_StatusTypeDef LLR_8Bits(uint8_t register_address, uint8_t *data_buffer);		// Read single-byte data from I2C device
-	HAL_StatusTypeDef LLR_16Bits(uint8_t register_address, uint16_t *data_buffer);	// Read multiple-byte data from I2C device
 
-	HAL_StatusTypeDef LLW_8Bits(uint8_t register_address, uint8_t data_buffer);		// Write single-byte data to I2C device
-	HAL_StatusTypeDef LLW_16Bits(uint8_t register_address, uint16_t data_buffer);	// Write multiple-byte to I2C device
+	// --- Low-level I2C methods --------------------------------------------------------
 
-	// Utility methods for bit operations
-	uint8_t mask_8Bits(uint8_t data, uint8_t mask, bool invert_mask = false);		// Apply a mask to the byte
-	uint16_t mask_16Bits(uint16_t data, uint16_t mask, bool invert_mask = false);	// Apply a mask to the bytes
+	// --- Read
+
+	HAL_StatusTypeDef LLR_8Bits(uint8_t register_address, uint8_t *data_buffer);
+	HAL_StatusTypeDef LLR_16Bits(uint8_t register_address, uint16_t *data_buffer);
+
+
+	// --- Write
+
+	HAL_StatusTypeDef LLW_8Bits(uint8_t register_address, uint8_t data_buffer);
+	HAL_StatusTypeDef LLW_16Bits(uint8_t register_address, uint16_t data_buffer);
+
+
+	// --- Utility methods for bit operations -------------------------------------------
+
+	uint8_t mask_8Bits(uint8_t data, uint8_t mask, bool invert_mask = false);
+	uint16_t mask_16Bits(uint16_t data, uint16_t mask, bool invert_mask = false);
+
 
 private:
-	// Utility methods for bit operations
-	uint16_t concat_8to16Bits(uint8_t *bytes);				// Concatenate two uint8_t to form a uint16_t
+	// --- Utility methods for bit operations -------------------------------------------
+
+	uint16_t concat_8to16Bits(uint8_t *bytes);				//
+
 	void break_16to8Bits(uint16_t bytes, uint8_t *result);	// Breaks a uint16_t to form two uint8_t
 };
 
 
-
-
-
-
+// END OF FILE
